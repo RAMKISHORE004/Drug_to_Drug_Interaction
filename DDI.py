47741ed9bcd3b936deafd3476e4365cfce7cd086
Ahana <ahanabandyopadhyay1998@gmail.com>
2025-02-11 18:55:12 -0500

    Intial Commit for Imports

--- DDI.py
+++ DDI.py
@@ -1,1 +1,10 @@
 #Drug to Drug Interaction code
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the dataset
+file_path = "C:/Users/ahana/OneDrive/Desktop/capstone/Drug_to_Drug_Interaction/DDICorpus2013.xlsx"
+df = pd.read_excel(file_path)
+print(df.head(10))
